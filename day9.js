18d9248b61b506d6790525111d6c73c408626d76
Vincent van der Weele <vincentvanderweele@hotmail.com>
2020-12-09 08:51:33 +0200

    Create day9.js

---
+++ day9.js
@@ -0,0 +1,34 @@
+data = document
+  .querySelector('pre')
+  .firstChild
+  .data
+  .split('\n')
+  .filter(x => x)
+  .map(x => parseInt(x))
+  
+// problem 1
+w=25
+t = data.filter((x, i) => 
+  i >= w 
+  && !data.slice(i-w, i)
+     .reduce((s, y) => { 
+       data.slice(i-w, i)
+         .forEach(z => s.add(y+z));
+       return s
+     }, new Set())
+     .has(x)
+  )[0]
+
+// problem 2
+i=0, j=0, s=0
+while(s !== t) {
+  if (s < t) { 
+    s+=data[j];
+    j++
+  } else {
+    s-=data[i];
+    i++;
+  }
+}
+r=data.slice(i, j).sort((a, b) => a-b)
+r[0]+r[r.length-1]
