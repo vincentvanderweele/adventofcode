5c2e39d83bc47a0f07f4cc01ff26da01094b0d04
Vincent van der Weele <vincentvanderweele@hotmail.com>
2020-12-10 12:20:08 +0200

    Update day10.js

--- day10.js
+++ day10.js
@@ -11,20 +11,19 @@ data = [0, ...data, data[data.length-1]+3]
   
 // problem 1
 r = data
-  .map((_, i, a) => i > 0 ? a[i] - a[i-1] : 0)
-  .filter(x => x)
+  .slice(1)
+  .map((x, i) => x - data[i])
   .reduce((r, x) => { r[x % 3]++; return r }, [0, 0])
 r[0]*r[1]
 
 // problem 2
 c = {}
 function s(d, i, l) {
-    if (i == d.length-1 && d[i] - l <= 3) return 1
     if (i >= d.length || d[i] - l > 3) return 0
+    if (i === d.length-1) return 1
 
     if (!c[i]) c[i] = {}
-    if (!c[i][l]) c[i][l] = s(d, i+1, l) + s(d, i+1, d[i])
-
     return c[i][l]
+      || (c[i][l] = s(d, i+1, l) + s(d, i+1, d[i]))
 }
 s(data, 1, 0)
