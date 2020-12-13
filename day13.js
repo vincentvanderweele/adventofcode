e739e7118558fa9b9aa1440cd8fab2d5f0f0012e
Vincent van der Weele <vincentvanderweele@hotmail.com>
2020-12-13 10:46:03 +0200

    Update day13.js

--- day13.js
+++ day13.js
@@ -1,18 +1,19 @@
-// problem 1
 target=1008713
 data='13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23'
+
+// problem 1
+data
   .split(',')
   .map(x=>parseInt(x))
   .filter(x=>!isNaN(x))
-data.map(x=>x-target%x)[2]*data[2]
+  .map(x=>x-target%x)[2]*467
 
 // problem 2
-data='13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23'
+data
   .split(',')
   .map((x, i) => ({o: i, v: parseInt(x)}))
   .filter(({v})=>!isNaN(v))
-data.reduce(({ o, v }, x) => {
-	i=o
-	while((i+x.o)%x.v) {i+=v}
-	return { o: i, v: v*x.v }
-})[o]
+  .reduce(({ o, v }, x) => {
+    while((o+x.o)%x.v) {o+=v}
+    return { o, v: v*x.v }
+  }).o
