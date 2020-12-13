9ee35d3042a8758b3514f7af9875e78f7860bb70
Vincent van der Weele <vincentvanderweele@hotmail.com>
2020-12-13 09:25:06 +0200

    Update day13.js

--- day13.js
+++ day13.js
@@ -4,7 +4,7 @@ data='13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,
   .split(',')
   .map(x=>parseInt(x))
   .filter(x=>!isNaN(x))
-data.map(x=>x-target%x)[2]*467
+data.map(x=>x-target%x)[2]*data[2]
 
 // problem 2
 data='13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23'
