1ce0f5e4eae6e27b1a2c4f07ecfca5a0ff11cc41
Vincent van der Weele <vincentvanderweele@hotmail.com>
2021-12-01 08:55:13 +0200

    day 1 2021

---
+++ 2021/day01.js
@@ -0,0 +1,18 @@
+data = document
+  .querySelector('pre')
+  .firstChild
+  .data
+  .split('\n')
+  .map(x => parseInt(x))
+  .filter(x => !isNaN(x))
+
+// problem 1
+data
+  .filter((x, i, a) => i > 0 && x > a[i-1])
+  .length
+
+// problem 2
+data
+  .map((x, i, a) => i < 2 ? NaN : x + a[i-1] + a[i-2])
+  .filter((x, i, a) => i > 0 && x > a[i-1])
+  .length
