b8fbe3c2e92e07b3c6c04703153d3c0a8ea64fc5
Vincent van der Weele <vincent.van.der.weele@futurice.com>
2021-12-03 10:13:56 +0200

    Enable prettier

--- 2021/day01.js
+++ 2021/day01.js
@@ -1,18 +1,13 @@
 data = document
   .querySelector('pre')
-  .firstChild
-  .data
-  .split('\n')
-  .map(x => parseInt(x))
-  .filter(x => !isNaN(x))
+  .firstChild.data.split('\n')
+  .map((x) => parseInt(x))
+  .filter((x) => !isNaN(x));
 
 // problem 1
-data
-  .filter((x, i, a) => i > 0 && x > a[i-1])
-  .length
+data.filter((x, i, a) => i > 0 && x > a[i - 1]).length;
 
 // problem 2
 data
-  .map((x, i, a) => i < 2 ? NaN : x + a[i-1] + a[i-2])
-  .filter((x, i, a) => i > 0 && x > a[i-1])
-  .length
+  .map((x, i, a) => (i < 2 ? NaN : x + a[i - 1] + a[i - 2]))
+  .filter((x, i, a) => i > 0 && x > a[i - 1]).length;
