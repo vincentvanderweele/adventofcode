839052c188f6d778fbebcecf5240b4d66ee9951e
Vincent van der Weele <vincent.van.der.weele@futurice.com>
2021-12-10 10:50:59 +0200

    Keep problem 1 scores out of general solution

--- 2021/day10.js
+++ 2021/day10.js
@@ -6,27 +6,29 @@ data = document
 
 open = '([{<';
 close = ')]}>';
-score = [3, 57, 1197, 25137];
 
-// compute scores (for problem 1) and remaining stacks (for problem 2)
+// compute invalid inputs (for problem 1) and remaining stacks (for problem 2)
 // the problems use different rows, so we can compute both in one go
-stacksAndScores = data.map((x) => {
+stacksAndInvalidInputs = data.map((x) => {
   stack = [];
   for (c of x) {
     if (open.includes(c)) {
       stack.push(open.indexOf(c));
     } else if (stack.pop() !== close.indexOf(c)) {
-      return score[close.indexOf(c)];
+      return close.indexOf(c);
     }
   }
   return stack;
 });
 
 // problem 1
-stacksAndScores.filter((x) => !Array.isArray(x)).reduce((s, e) => s + e, 0);
+score = [3, 57, 1197, 25137];
+stacksAndInvalidInputs
+  .filter((x) => !Array.isArray(x))
+  .reduce((s, e) => s + score[e], 0);
 
 // problem 2
-filtered = stacksAndScores.filter((x) => x.length > 0);
+filtered = stacksAndInvalidInputs.filter((x) => x.length > 0);
 filtered
   .map((x) => x.reverse().reduce((s, e) => 5 * s + e + 1, 0))
   .sort((x, y) => x - y)[(filtered.length - 1) / 2];
