1202f46a6a1d66e03acc97d014f344a921180790
Vincent van der Weele <vincent.van.der.weele@futurice.com>
2021-12-17 10:24:08 +0200

    Add day 17

---
+++ 2021/day17.js
@@ -0,0 +1,40 @@
+[xMin, xMax, yMin, yMax] = document
+  .querySelector('pre')
+  .firstChild.textContent.trim()
+  .slice('target area: '.length)
+  .split(', ')
+  .flatMap((x) =>
+    x
+      .slice(2)
+      .split('..')
+      .map((x) => parseInt(x))
+  );
+
+// problem 1
+[...Array(-yMin)].reduce((max, _, vy0) => {
+  [vy, y, maxY] = [vy0, 0, 0];
+  while (y > yMax) {
+    y += vy--;
+    maxY = Math.max(y, maxY);
+  }
+  return y >= yMin && maxY > max ? maxY : max;
+}, 0);
+
+// problem 2
+reaches = (vx0, vy0) => {
+  [vx, vy, x, y] = [vx0, vy0, 0, 0];
+  while ((vx && x < xMin) || y > yMax) {
+    x += vx;
+    y += vy--;
+    vx = Math.max(vx - 1, 0);
+  }
+  return x >= xMin && x <= xMax && y >= yMin;
+};
+
+[...Array(xMax + 1)]
+  .flatMap((_, vx0) =>
+    [...Array(-2 * yMin + 1)]
+      .map((_, vy0) => vy0 + yMin)
+      .map((vy0) => reaches(vx0, vy0))
+  )
+  .filter((x) => x).length;
