87fb175e13472d9f8c8207de0b8493b5d5af47b8
Vincent van der Weele <vincentvanderweele@hotmail.com>
2025-12-03 09:10:31 +0200

    Reuse same solution

--- 2025/day03.js
+++ 2025/day03.js
@@ -4,28 +4,19 @@ input = document
   .filter(x => x)
   .map(x => x.split('').map(x => parseInt(x, 10)));
 
-a = input
-  .map(([m1, m2, ...r]) =>
-    r.reduce(
-      ([m1, m2], x) => (m2 > m1 ? [m2, x] : [m1, Math.max(m2, x)]),
-      [m1, m2]
-    )
-  )
-  .reduce((s, [x1, x2]) => s + 10 * x1 + x2, 0);
-
-b = input
+solve = n => input
   .map(a =>
-    a.slice(12).reduce((m, x) => {
+    a.slice(n).reduce((m, x) => {
       m.push(x);
-      for (i = 0; i < 12; i++) {
+      for (i = 0; i < n; i++) {
         if (m[i + 1] > m[i]) {
           m.splice(i, 1);
           break;
         }
       }
-      return m.slice(0, 12);
-    }, a.slice(0, 12))
+      return m.slice(0, n);
+    }, a.slice(0, n))
   )
   .reduce((s, x) => s + x.reduce((r, x) => 10 * r + x), 0);
 
-[a, b];
+[solve(2), solve(12)]; 
